961845e495407a96eb93db5e9c6666db3e4d5f19
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-05-15 10:36:42 +0200

    Update home.py

--- home.py
+++ home.py
@@ -3,9 +3,10 @@ import streamlit as st
 def main():
     st.title('BacTracker')
     st.write('Welcome to your BacTracker App')
+    st.image("static/bakterien.jpeg", caption="Image of bacteria", use_column_width=True)
     st.page_link("home.py", label="Home", icon="🏠")
-    st.page_link("pages/statistik.py", label="Statistik")
-    st.page_link("pages/datenbank.py", label="Datenbank")
-    
+    st.page_link("pages/Statistik.py", label="Statistik", icon="📊")
+    st.page_link("pages/Datenbank.py", label="Datenbank")
+
 if __name__ == "__main__":
     main()
