6741ff516481544f25e697cba143f493898fbb48
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-05-15 10:21:51 +0200

    Update home.py

--- home.py
+++ home.py
@@ -4,8 +4,8 @@ def main():
     st.title('BacTracker')
     st.write('Welcome to your BacTracker App')
     st.page_link("home.py", label="Home", icon="🏠")
-    st.page_link("pages/Statistik.py", label="Statistik")
-    st.page_link("pages/Datenbank.py", label="Datenbank")
+    st.page_link("pages/statistik.py", label="Statistik")
+    st.page_link("pages/datenbank.py", label="Datenbank")
     
 if __name__ == "__main__":
     main()
