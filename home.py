717dfd37bad0ef6a9442b3fcbe7002a959d7a418
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-05-15 10:16:43 +0200

    Update home.py

--- home.py
+++ home.py
@@ -6,13 +6,6 @@ def main():
     st.page_link("home.py", label="Home", icon="🏠")
     st.page_link("pages/Statistik.py", label="Statistik")
     st.page_link("pages/Datenbank.py", label="Datenbank")
-
-    if st.button("Home"):
-        st.switch_page("home.py")
-    if st.button("Statistik"):
-        st.switch_page("pages/Statistik.py")
-    if st.button("Datenbank"):
-        st.switch_page("pages/Datenbank.py")
     
 if __name__ == "__main__":
     main()
