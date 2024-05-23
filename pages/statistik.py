6d6c2ae158aff0a3fcd9923a881ed2b9f3e75059
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-05-23 10:41:23 +0200

    Update statistik.py

--- pages/statistik.py
+++ pages/statistik.py
@@ -170,22 +170,20 @@ def main_statistik():
             st.write("Keine Daten zum Anzeigen.")
 
     with tab2:
-        st.header("Anzahl")
-        if not st.session_state.df.empty:  # Check if the dataframe is not empty
-            col1, col2 = st.columns(2)
-
-        with col1:
-            st.header("Tabelle")
-            display_dataframe()
-
-        with col2:
-            st.header("Anzahl")
-            total_entries, total_pathogenic, percent_pathogenic = calculate_statistics()
-            st.write(f"Gesamte Einträge: {total_entries}")
-            st.write(f"Anzahl Pathogen: {total_pathogenic}")
-            st.write(f"Prozentualer Anteil Pathogen: {percent_pathogenic:.2f}%")
+    st.header("Plot")
+    plotx = st.radio("X-Achse", ["Pathogenität", "Platten", "Material"])
+    if not st.session_state.df.empty:  # Check if the dataframe is not empty
+        if plotx == "Pathogenität":
+            data = st.session_state.df["Pathogenität"].value_counts().reset_index()  # Corrected column name here
+            data.columns = ["Pathogenität", "Count"]
+            st.bar_chart(data.set_index(data.columns[0]))
         else:
-            st.write("Keine Daten zum Anzeigen.")
+            data = st.session_state.df["Material"].value_counts().reset_index()
+            data.columns = ["Material", "Count"]
+            st.bar_chart(data.set_index(data.columns[0]))
+    else:
+        st.write("Keine Daten zum Anzeigen.")
+
 
     st.header("Plot")
     plotx = st.radio("X-Achse", ["Pathogenität", "Platten", "Material"])
