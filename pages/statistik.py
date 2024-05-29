9fe6328c6a7f75e9d6f19a5c71266470438579bf
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-05-29 20:27:09 +0200

    Update statistik.py

--- pages/statistik.py
+++ pages/statistik.py
@@ -38,7 +38,8 @@ def login_page():
     with st.form(key='login_form'):
         username = st.text_input("Username", key="login_username")
         password = st.text_input("Password", type="password", key="login_password")
-        if st.form_submit_button("Login", key="login_submit"):
+        submit_button = st.form_submit_button("Login")
+        if submit_button:
             authenticate(username, password)
 
 def register_page():
@@ -48,7 +49,8 @@ def register_page():
         new_username = st.text_input("New Username", key="register_username")
         new_name = st.text_input("Name", key="register_name")
         new_password = st.text_input("New Password", type="password", key="register_password")
-        if st.form_submit_button("Register", key="register_submit"):
+        submit_button = st.form_submit_button("Register")
+        if submit_button:
             hashed_password = bcrypt.hashpw(new_password.encode('utf8'), bcrypt.gensalt())
             hashed_password_hex = binascii.hexlify(hashed_password).decode()
             
