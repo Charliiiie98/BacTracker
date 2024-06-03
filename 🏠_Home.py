1468f50685eb5188b4c7b768d30b38f3e792261c
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 14:18:47 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -5,7 +5,19 @@ st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 def main():
     st.title('BacTracker')
     st.write('Welcome to your BacTracker App')
-    st.image("statics/bakterien.jpeg", caption="Image of bacteria", use_column_width=True)
+
+    # CSS for setting background image
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background-image: ('statics/bakterien.jpeg');
+            background-size: cover;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 if __name__ == "__main__":
-        main()
+    main()
