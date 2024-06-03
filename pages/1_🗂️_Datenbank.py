6cbe538a26705cfca919163f9979ab36c1968369
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 19:25:21 +0200

    Update 1_🗂️_Datenbank.py

--- pages/1_🗂️_Datenbank.py
+++ pages/1_🗂️_Datenbank.py
@@ -50,7 +50,7 @@ def main():
         if selected_characterizations:
             filtered_df = filtered_df[
                 filtered_df.apply(
-                    lambda row: all(char in ' '.join([str(row['Charakerisierung']), str(row['Charakerisierung2']), str(row['Charakerisierung3'])]) for char in selected_characterizations), 
+                    lambda row: all(char in ' '.join([str(row['Charakerisierung1']), str(row['Charakerisierung2']), str(row['Charakerisierung3'])]) for char in selected_characterizations), 
                     axis=1
                 )
             ]
