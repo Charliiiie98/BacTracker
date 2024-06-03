069658c44ed296ccc2746af7a7b0b25c38449b68
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 14:10:46 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,27 +1,11 @@
 import streamlit as st
 
-# Set page configuration
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
-# Define a function to handle the logout process
-def logout():
-    st.session_state['logged_in'] = False
-
-# Define the main function
 def main():
-    # Check if the user is logged in
-    if 'logged_in' not in st.session_state:
-        st.session_state['logged_in'] = True
+    st.title('BacTracker')
+    st.write('Welcome to your BacTracker App')
+    st.image("statics/bakterien.jpeg", caption="Image of bacteria", use_column_width=True)
 
-    if st.session_state['logged_in']:
-        st.sidebar.button('Logout', on_click=logout)
-        st.title('BacTracker')
-        st.write('Welcome to your BacTracker App')
-        st.image("statics/bakterien.jpeg", caption="Image of bacteria", use_column_width=True)
-    else:
-        st.sidebar.write('You have been logged out.')
-        st.write('Please log in to access the BacTracker App.')
-
-# Run the main function
 if __name__ == "__main__":
-    main()
+        main()
