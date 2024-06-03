85e5d73b9d38d30b13a1b34797e0803cfe99a239
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 14:41:13 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from st_pages import hide_pages
 
-hide_pages(["pages/login"])
+hide_pages(["pages/login.py"])
 
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
