d9630c381693830c9c9132a0205b39b5da54c554
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 13:49:57 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -7,43 +7,20 @@ st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 def logout():
     st.session_state['logged_in'] = False
 
-# Define a function to set up the sidebar
-def setup_sidebar():
+# Define the main function
+def main():
+    # Check if the user is logged in
     if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = True
+
     if st.session_state['logged_in']:
         st.sidebar.button('Logout', on_click=logout)
-
-# Define the main function for the home page
-def home():
-    setup_sidebar()
-    if st.session_state['logged_in']:
         st.title('BacTracker')
         st.write('Welcome to your BacTracker App')
         st.image("statics/bakterien.jpeg", caption="Image of bacteria", use_column_width=True)
     else:
+        st.sidebar.write('You have been logged out.')
         st.write('Please log in to access the BacTracker App.')
-
-# Define other pages if needed
-def other_page():
-    setup_sidebar()
-    if st.session_state['logged_in']:
-        st.title('Other Page')
-        st.write('This is another page in your app.')
-    else:
-        st.write('Please log in to access this page.')
-
-# Define a function to handle navigation
-def main():
-    # Create a sidebar menu
-    menu = ['Home', 'Other Page']
-    choice = st.sidebar.selectbox('Navigation', menu)
-
-    # Navigate to the selected page
-    if choice == 'Home':
-        home()
-    elif choice == 'Other Page':
-        other_page()
 
 # Run the main function
 if __name__ == "__main__":
