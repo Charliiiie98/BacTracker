b568c0c8edb86501e9109dc38ad70833e65e8708
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 11:57:49 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -1,30 +1,6 @@
 import streamlit as st
-import pandas as pd
-from funktions.github_contents import GithubContents
 
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
-
-DATA_FILE = "MyLoginTable.csv"
-DATA_COLUMNS = ['username', 'name', 'password']
-
-def init_github():
-    """Initialize the GithubContents object."""
-    if 'github' not in st.session_state:
-        st.session_state.github = GithubContents(
-            st.secrets["github"]["owner"],
-            st.secrets["github"]["repo"],
-            st.secrets["github"]["token"])
-
-def init_credentials():
-    """Initialize or load the dataframe."""
-    if 'df_users' in st.session_state:
-        pass
-
-    if st.session_state.github.file_exists(DATA_FILE):
-        st.session_state.df_users = st.session_state.github.read_df(DATA_FILE)
-    else:
-        st.session_state.df_users = pd.DataFrame(columns=DATA_COLUMNS)
-
 
 def main():
     init_github()
