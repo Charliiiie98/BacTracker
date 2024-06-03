a80c2bac248b7e15f7130a94b3d49e4afbe3199c
Charliiiie98 <162136967+Charliiiie98@users.noreply.github.com>
2024-06-03 11:58:25 +0200

    Update 🏠_Home.py

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -3,8 +3,6 @@ import streamlit as st
 st.set_page_config(page_title="Home", page_icon="🏠", layout="wide")
 
 def main():
-    init_github()
-    init_credentials()
     st.title('BacTracker')
     st.write('Welcome to your BacTracker App')
     st.image("statics/bakterien.jpeg", caption="Image of bacteria", use_column_width=True)
